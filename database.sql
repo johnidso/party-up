faeeccc2de2edcb451d09a02e1b1d2531535ba09
John Idso <john.d.idso@gmail.com>
2021-08-02 21:34:13 -0500

    added LibraryList and LibraryItem to display a users own steam library cards

--- database.sql
+++ database.sql
@@ -1,10 +1,17 @@
 
--- USER is a reserved keyword with Postgres
--- You must use double quotes in every query that user is in:
--- ex. SELECT * FROM "user";
--- Otherwise you will have errors!
 CREATE TABLE "users" (
-    "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
-);
+	"id" SERIAL PRIMARY KEY, 
+	"username" VARCHAR(20) NOT NULL, 
+	"password" VARCHAR(1000) NOT NULL, 
+	"email" VARCHAR(30) NOT NULL, 
+	"steam_id" VARCHAR(70) NOT NULL, 
+	"discord_id" VARCHAR(30)
+	);
+
+CREATE TABLE "steam-info" (
+	"id" SERIAL PRIMARY KEY,
+	"steam_id" VARCHAR(70) NOT NULL,
+	"avatar" VARCHAR(255) NOT NULL,
+	"persona" VARCHAR(50) NOT NULL,
+	"profile_url" VARCHAR(255) NOT NULL
+	);
---
+++ src/components/LibraryItem/LibraryItem.jsx
@@ -0,0 +1,14 @@
+import { useDispatch } from "react-redux"
+
+
+function LibraryItem(props) {
+
+
+    return(
+        <section className="nes-container">
+
+        </section>
+    )
+}
+
+export default LibraryItem;
