6707042ba0e1b5e88d3e9968655b738acc6299ec
John Idso <john.d.idso@gmail.com>
2021-08-05 15:56:52 -0500

    issue with material ui date picker

--- src/components/CreateEvent/CreateEvent.jsx
+++ src/components/CreateEvent/CreateEvent.jsx
@@ -1,13 +1,26 @@
-import { useParams } from "react-router-dom"
+import * as React from 'react';
+import TextField from '@material-ui/core/TextField';
+import AdapterMoment from '@material-ui/lab/AdapterMoment';
+import LocalizationProvider from '@material-ui/lab/LocalizationProvider';
+import DateTimePicker from '@material-ui/lab/DateTimePicker';
+import { useParams } from "react-router-dom";
 
 
 function CreateEvent() {
+    const date = new Date();
     const { attendeeId, attendeeName, gameId, gameImage } = useParams();
     return(
-        <>
-            <h2>New Event with {attendeeName}:</h2>
-            <input className="nes-input"></input>
-        </>
+            <LocalizationProvider dateAdapter={AdapterMoment}>
+                <h2>New Event with {attendeeName}:</h2>
+                <input className="nes-input" ></input>
+                <MobileDateTimePicker
+                    value={value}
+                    onChange={(newValue) => {
+                        setValue(newValue);
+                    }}
+                    renderInput={(params) => <TextField {...params} />}
+                    />
+            </LocalizationProvider>
     )
 }
 
