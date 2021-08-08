040d9cf807cf8d13984576b2254ca4b93b20840a
John Idso <john.d.idso@gmail.com>
2021-08-08 17:24:39 -0500

    Game search working case-insensitively, added clear button for search results

--- src/components/PlaylistPage/LibraryList/LibraryList.jsx
+++ src/components/PlaylistPage/LibraryList/LibraryList.jsx
@@ -6,6 +6,7 @@ import LibraryItem from "../LibraryItem/LibraryItem";
 function LibraryList() {
     const library = useSelector(store => store.library);
     const [search, setSearch] = useState('');
+    const [searchResults, setSearchResults] = useState([]);
     let emptySearch = '';
 
     const handleChange = event => {
@@ -13,13 +14,42 @@ function LibraryList() {
         setSearch(value);
     }
 
+    const searchLibrary = (searchTerm) => {
+        let search = searchTerm.toUpperCase();
+        let results = [];
+        for (let game of library) {
+            let gameName =  game.name.toUpperCase();
+            if (search && gameName.includes(search)) {
+                results.push(game);
+            }
+        }
+        setSearchResults(results);
+        console.log(results);
+    }
+
+    const handleSearch = () => {
+        searchLibrary(search);
+        setSearch(emptySearch);
+    }
+
+    const clearSearch = () => {
+        setSearchResults([]);
+    }
+
     return(
         <section>
             <h1>My Library</h1>
             <section id='searchSection'>
             <input type="text" id="searchIn" value={search} className="nes-input" placeholder="search games" onChange={handleChange}></input>
-            <button className="nes-btn" id="searchButton" >Search</button>
+            <button className="nes-btn" id="clearButton" onClick={clearSearch}>Clear</button>
+            <button className="nes-btn" id="searchButton" onClick={handleSearch}>Search</button>
+            {searchResults.map(game => {                
+                return (
+                    <LibraryItem key={game.appid} gameId={game.appid} img={game.img_logo_url} />
+                )                
+            })}
             </section>
+            <h1>Recently Played</h1>
             {library.map(game => {
                 if (game.playtime_2weeks) {
                     return (
--- src/components/PlaylistPage/PlaylistPage.jsx
+++ src/components/PlaylistPage/PlaylistPage.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux";
 import { bindActionCreators } from "redux";
-import LibraryList from '../LibraryList/LibraryList';
+import LibraryList from "./LibraryList/LibraryList";
 import './PlaylistPage.css';
 
 function PlaylistPage() {
